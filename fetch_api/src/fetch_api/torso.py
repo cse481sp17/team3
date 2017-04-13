815d6a86434433a19bf80b31aaf45ed7a0f84249
Team 3 <yenlee14@uw.edu>
2017-04-13 13:28:38 -0700

    Moving to different heights works

--- fetch_api/src/fetch_api/torso.py
+++ fetch_api/src/fetch_api/torso.py
@@ -4,9 +4,13 @@
 # TODO: import ???????_msgs.msg
 # TODO: import ??????????_msgs.msg
 import rospy
+import actionlib
+import control_msgs.msg
+import trajectory_msgs.msg
 
-# TODO: ACTION_NAME = ???
-# TODO: JOINT_NAME = ???
+
+ACTION_NAME = 'torso_controller/follow_joint_trajectory'
+JOINT_NAME = 'torso_lift_joint'
 TIME_FROM_START = 5  # How many seconds it should take to set the torso height.
 
 
@@ -17,9 +21,8 @@ class Torso(object):
     MAX_HEIGHT = 0.4
 
     def __init__(self):
-        # TODO: Create actionlib client
-        # TODO: Wait for server
-        pass
+	self.client = actionlib.SimpleActionClient(ACTION_NAME, control_msgs.msg.FollowJointTrajectoryAction)
+	self.client.wait_for_server()
 
     def set_height(self, height):
         """Sets the torso height.
@@ -30,15 +33,26 @@ class Torso(object):
             height: The height, in meters, to set the torso to. Values range
                 from Torso.MIN_HEIGHT (0.0) to Torso.MAX_HEIGHT(0.4).
         """
-        # TODO: Check that the height is between MIN_HEIGHT and MAX_HEIGHT.
-        # TODO: Create a trajectory point
-        # TODO: Set position of trajectory point
-        # TODO: Set time of trajectory point
+	if height <= self.MAX_HEIGHT and height >= self.MIN_HEIGHT:
+		jtp = trajectory_msgs.msg.JointTrajectoryPoint()
+		jtp.positions.append(height)
+		jtp.time_from_start = rospy.Duration(TIME_FROM_START)
 
-        # TODO: Create goal
-        # TODO: Add joint name to list
-        # TODO: Add the trajectory point created above to trajectory
+		goal = control_msgs.msg.FollowJointTrajectoryGoal()
+		goal.trajectory.joint_names.append(JOINT_NAME)
+		goal.trajectory.points.append(jtp)
 
-        # TODO: Send goal
-        # TODO: Wait for result
-        rospy.logerr('Not implemented.')
+		self.client.send_goal(goal)	
+		self.client.wait_for_result()
+
+
+
+
+
+
+
+
+
+
+
+
